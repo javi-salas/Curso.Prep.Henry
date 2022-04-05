f1b1c448d6b656f16c191f9b1bbd8cf528ab29bd
javi-salas <javier.esteban.salas@gmail.com>
2022-04-05 02:19:59 -0300

    homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -58,7 +58,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  var palabras = ['hello', 'world!'];
+  var palabras = ['hello',' world!'];
   return palabras[0] + palabras[1];
 }
 
